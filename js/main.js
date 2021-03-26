d07838e1ad0f727623be6a318b087dae1c4cb741
Wing Wong <wingyiu.wong@displaylink.com>
2021-03-26 13:50:42 +0000

    main.js - added convert input to pennies function

--- js/main.js
+++ js/main.js
@@ -29,11 +29,50 @@ $(function(){
             ){                        
                 alert('Your input is empty or contains invalid character(s)')   
             } else {    
-                alert('proceed to process input')
+                calculator.convertToPennies(input);  
             }
         },
 
-        
+        convertToPennies : function(input){
+
+            let totalInPennies;
+
+            function removeNonNumChars(input){
+                return input.replace(/[^0-9$.,]/g, '');
+            }
+
+            //1.CHECK FOR '£' OR '.' IN INPUT.     
+            if(input.indexOf('£') >= 0 && input.indexOf('.') == -1) { //POUNDS ONLY
+            
+                let pounds = removeNonNumChars(input);
+                totalInPennies = (parseInt(pounds) * 100)
+
+            } else if(input.indexOf('.') >= 0){ //POUNDS AND PENNIES
+
+                input = removeNonNumChars(input);
+                
+                let arrValue = input.split(".");
+                let pounds = parseInt(arrValue[0]);
+                let pennies = parseInt(arrValue[1]) || 0;
+                
+                if(pennies > 99){
+                    pennies = parseFloat('0.'+pennies).toFixed(2)
+                    let arrPennies = pennies.split('.');
+                    pennies = Math.ceil(arrPennies[1]);
+                }
+            
+                totalInPennies = (100 * pounds) + pennies;
+                
+            } else { //PENNIES ONLY
+                input = removeNonNumChars(input);
+                totalInPennies = parseInt(input);
+            }
+
+            alert('Proceed to convert to coins')
+
+        },
+
+       
     }
 
     calculator.init()
